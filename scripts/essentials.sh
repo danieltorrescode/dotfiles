8c80422ea710d12d7102b057efcc6fe703512e11
Daniel Torres <danieltorres2902@gmail.com>
2020-04-16 13:37:36 -0400

    updated .gitignore rules

--- scripts/essentials.sh
+++ scripts/essentials.sh
@@ -5,7 +5,7 @@ echo "###########################################"
 apt-get update
 apt-get install -y aptitude
 aptitude install -y build-essential screen curl htop wget ranger xorg 
-aptitude install -y xdo xtitle gcc-c++ clang git cmake
+aptitude install -y xdo xtitle clang git cmake
 aptitude install -y vim vim-syntastic vim-snippets vim-youcompleteme
 aptitude install -y emacs emacs-goodies-el
 aptitude install -y compton feh arandr playerctl pavucontrol
@@ -14,5 +14,5 @@ aptitude install -y zathura mpv imagemagick
 aptitude install -y python-pip python3-pip virtualenv
 aptitude install -y pkg-config libfontconfig-dev libdbus-glib-1-2 libfreetype6-dev
 aptitude install -y libx11-dev libxft-dev libxinerama1 libxinerama-dev
-aptitude install -y simple-scan
+aptitude install -y simple-scan fonts-hack-ttf fonts-font-awesome fonts-liberation
 
