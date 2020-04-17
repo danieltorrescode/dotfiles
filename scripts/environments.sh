05014b4fa3d2830072e5e97457373dc8fa6582ea
Daniel Torres <danieltorres2902@gmail.com>
2020-04-17 13:07:03 -0400

    changed deployment method

--- scripts/environments.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "###########################################" 
-echo "installing containers and virtual machine tools" 
-echo "###########################################" 
-
-aptitude install -y qemu qemu-kvm docker.io docker-compose  
-#aptitude install -y ufw gufw firejail apparmor lxc
-#aptitude install -y i3-wm i3blocks i3lock 
-
